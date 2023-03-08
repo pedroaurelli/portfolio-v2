2bbba9d215a0814ae0442e3d8b8ba6ecc15fc33d
pedroaurelli <pedroaureliano.contato@gmail.com>
2023-03-08 16:32:37 -0300

    Add eslint setup to project

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,9 @@
+import React from 'react'
 import './styles/global.css'
 
 function App() {
   return (
-    <></>
+    <>hello</>
   )
 }
 
--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,7 +1,8 @@
 /** @type {import('tailwindcss').Config} */
+// eslint-disable-next-line no-undef
 module.exports = {
   content: [
-    "./src/**/*.{tsx,ts,jsx,js}"
+    './src/**/*.{tsx,ts,jsx,js}'
   ],
   theme: {
     extend: {},
