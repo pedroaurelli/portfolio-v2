74a54a02f183dbd5f3be172f76a6340b9e4642e8
pedroaurelli <pedroaureliano.contato@gmail.com>
2023-03-13 20:28:01 -0300

    Update text copy

--- src/components/AboutMeSection/AboutMeSection.tsx
+++ src/components/AboutMeSection/AboutMeSection.tsx
@@ -11,32 +11,34 @@ export default function AboutMeSection() {
           <div className='flex flex-col-reverse tablet:flex-row  items-center justify-center gap-16 tablet:gap-32'>
             <div className='flex flex-col'>
               <ScrollDownToUp delay={0.4}>
-                <h2 className='text-[40px] mb-16'>Lorem ipsum sit dolor</h2>
+                <h2 className='text-[40px] mb-16'>Olá! Meu nome é Pedro!</h2>
               </ScrollDownToUp>
               <ScrollDownToUp delay={0.6}>
                 <p className='max-w-[523px]'>
-                  Lorem ipsum dolor sit amet consectetur. Diam id pulvinar lorem vel lorem id libero nibh lacus.
-                  Mi quam id ullamcorper nisi pharetra sed enim tincidunt. Lorem ipsum dolor sit amet consectetur.
-                  Diam id pulvinar lorem vel lorem id libero nibh lacus. Mi quam id ullamcorper nisi pharetra sed enim tincidunt.
+                  Estou na área de desenvolvimento de software desde janeiro de 2022,
+                  atuo como <a href='https://blog.rocketseat.com.br/do-back-ao-mobile-de-onde-surgiu-a-programacao-fullstack/#:~:text=E%20%C3%A9%20aqui%20que%20entra,para%20garantir%20estabilidade%20no%20sistema.' target='_blank' rel="noreferrer">
+                    <b>Desenvolvedor Full Stack Junior</b></a>, gosto de trabalhar com programação web, porém sempre aberto para conhecer e aprender novas stacks.
+
                 </p>
               </ScrollDownToUp>
               <br/>
               <ScrollDownToUp delay={0.8}>
                 <p className='max-w-[523px]'>
-                  Lorem ipsum dolor sit amet consectetur. Diam id pulvinar lorem vel lorem id libero nibh lacus.
-                  Mi quam id ullamcorper nisi pharetra sed enim tincidunt. Lorem ipsum dolor sit amet consectetur.
-                  Diam id pulvinar lorem vel lorem id libero nibh lacus. Mi quam id ullamcorper nisi pharetra sed enim tincidunt.
+                  Minha jornada no mundo de TI se inicia após eu concluir o ensino médio em 2020, comecei a estudar programação
+                  durante a pandemia do Covid-19, em 2021. Hoje cursando Análise e Desenvolvimento de Sistemas na Universidade
+                  São Judas Tadeu (USJT) em São Paulo - SP. Além de novos conhecimentos que acabo adquirindo no trabalho, gosto
+                  de dedicar meu tempo de modo autodita para estudar e aprender novas coisas.
                 </p>
               </ScrollDownToUp>
             </div>
             <RightToLeftView>
               <div className='flex flex-col items-center tablet:gap-12'>
                 <img src='https://github.com/pedroaurelli.png' className='rounded-full w-[77px] tablet:w-[180px] laptop:w-[260px]' />
-                <Button href={''} text='Call to action' className='hidden tablet:block'/>
+                <Button href={'./curriculo.pdf'} blank text='Visualizar CV' className='hidden tablet:block'/>
               </div>
             </RightToLeftView>
             <div className='block tablet:hidden order-first'>
-              <Button href={''} text='Call to action'/>
+              <Button href={'./curriculo.pdf'} text='Visualizar CV'/>
             </div>
           </div>
           <div className='mt-16 tablet:absolute tablet:bottom-16'>
--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -5,20 +5,13 @@ export default function Header() {
   return (
     <>
       <header className='container h-screen mx-auto flex items-center justify-center'>
-        <div className='tablet:flex tablet:max-w-4xl space-x-[40px]'>
+        <div className='tablet:flex tablet:max-w-4xl space-x-[40px] items-center justify-center'>
           <div className='flex flex-col w-full'>
             <DownToUp>
               <h1>Pedro Aureliano</h1>
             </DownToUp>
             <DownToUp delay={0.3}>
               <h2 className='mt-3'>Full Stack Developer.</h2>
-            </DownToUp>
-            <DownToUp delay={0.5}>
-              <h3 className='mt-16'>
-                Lorem ipsum dolor sit amet consectetur. Ipsum non in elementum arcu
-                in sed aliquet vel. Lorem ipsum dolor sit amet consectetur. Ipsum non
-                in elementum arcu in sed aliquet vel.
-              </h3>
             </DownToUp>
           </div>
           <div className='hidden tablet:block'>
