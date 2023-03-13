933f1d9f4f5060723f5e654f46461d477d977321
pedroaurelli <pedroaureliano.contato@gmail.com>
2023-03-13 20:27:17 -0300

    Add blank prop in button component

--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -2,13 +2,14 @@ type ButtonProps = {
   href: string
   text: string
   className?: string
+  blank?: boolean
 }
 
 export default function Button(props: ButtonProps) {
-  const { href, text, className: styles } = props
+  const { href, text, className: styles, blank } = props
 
   return (
-    <a href={href}>
+    <a href={href} target={blank ? '_blank' : '_self'} rel="noreferrer">
       <button className={`button group ${styles} transition-hover`}>
         <span className='group-hover:text-white transition-hover'>{text}</span>
       </button>
